1b1320582bfe7b4c7e03d0c3975a54f4be16869b
Rui Fernandes <rfernandes@confluent.io>
2024-03-13 11:06:45 +0100

    streams refactor example2 leveraging streams and optimizing state store usage

---
+++ kafkaStreamsRefactor2/README.md
@@ -0,0 +1,46 @@
+# Best Practice: Beyond Connect, Leverage Streams
+
+This is the example following now not only the best practice for leveraging Kafka Connect to sink data externally to 
+mongodb but also to use Streams as much as possible and not only use Tables so to optimize memory usage (state store 
+minimization). This assumes that updates on customer and product data are not as frequent as the ones on the orders 
+topic.
+
+Once you have the 3 datagen source connectors running as per global README, you can run the Kafka Streams application.
+
+Just run the io.confluent.developer.App class.
+
+Also run the product and customer consumer apps which will update Mongodb directly in case of updates on the 
+corresponding topics.
+
+You should see the new topic orders-enriched2 with the data from the orders topic enriched with the customer and product 
+data from the other topics. And also customer and product inner data getting updated when new updates arrive at 
+corresponding topics.
+
+Now we can configure the sink connector to sink the data to the mongodb database.
+
+```bash
+curl -i -X PUT -H "Accept:application/json" \
+    -H  "Content-Type:application/json" http://localhost:8083/connectors/my-sink-mongodb2/config \
+    -d '{
+          "connector.class"    : "com.mongodb.kafka.connect.MongoSinkConnector",
+          "connection.uri"     : "mongodb://root:example@mongo:27017",
+          "topics"             : "orders-enriched2",
+          "tasks.max"          : "1",
+          "auto.create"        : "true",
+          "auto.evolve"        : "true",
+          "database"           : "demo",
+          "value.converter.schema.registry.url": "http://schema-registry:8081",
+          "key.converter"       : "org.apache.kafka.connect.storage.StringConverter",
+          "value.converter"     : "io.confluent.connect.avro.AvroConverter",
+          "mongodb.delete.on.null.values": "true",
+          "delete.on.null.values": "true",
+          "document.id.strategy.overwrite.existing": "true",
+          "document.id.strategy": "com.mongodb.kafka.connect.sink.processor.id.strategy.ProvidedInKeyStrategy",
+          "transforms":"hk",
+          "transforms.hk.type"  : "org.apache.kafka.connect.transforms.HoistField$Key",
+          "transforms.hk.field" : "_id",
+          "writemodel.strategy" : "com.mongodb.kafka.connect.sink.writemodel.strategy.ReplaceOneDefaultStrategy",
+          "collection"          : "orders2"}'
+```
+
+Let's check the data in the mongodb database in http://localhost:18081 for the new collection orders2.
---
+++ kafkaStreamsRefactor2/src/main/java/io/confluent/developer/App.java
@@ -0,0 +1,160 @@
+package io.confluent.developer;
+
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.*;
+import shoes.shoe_customers;
+import shoes.shoe_orders;
+import shoes.shoe_orders_customers;
+import shoes.shoe_product;
+import shoes.shoe_orders_customers_products;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class App {
+
+
+    private final static String DEV_CONFIG_FILE = "configuration/dev.properties";
+
+    private SpecificAvroSerde<shoe_orders> buildOrdersSerde(final Properties allProps) {
+        final SpecificAvroSerde<shoe_orders> serde = new SpecificAvroSerde<>();
+        final Map<String, String> config = (Map) allProps;
+        serde.configure(config, false);
+        return serde;
+    }
+
+    private SpecificAvroSerde<shoe_customers> buildCustomersSerde(final Properties allProps) {
+        final SpecificAvroSerde<shoe_customers> serde = new SpecificAvroSerde<>();
+        final Map<String, String> config = (Map) allProps;
+        serde.configure(config, false);
+        return serde;
+    }
+
+    private SpecificAvroSerde<shoe_product> buildProductsSerde(Properties allProps) {
+        final SpecificAvroSerde<shoe_product> serde = new SpecificAvroSerde<>();
+        final Map<String, String> config = (Map) allProps;
+        serde.configure(config, false);
+        return serde;
+    }
+
+    private SpecificAvroSerde<shoe_orders_customers> buildOrdersCustomersSerde(Properties allProps) {
+        final SpecificAvroSerde<shoe_orders_customers> serde = new SpecificAvroSerde<>();
+        final Map<String, String> config = (Map) allProps;
+        serde.configure(config, false);
+        return serde;
+    }
+
+    private SpecificAvroSerde<shoe_orders_customers_products> buildOrdersCustomersProductsSerde(Properties allProps) {
+        final SpecificAvroSerde<shoe_orders_customers_products> serde = new SpecificAvroSerde<>();
+        final Map<String, String> config = (Map) allProps;
+        serde.configure(config, false);
+        return serde;
+    }
+
+    public Topology buildTopology(Properties allProps,
+                                  final SpecificAvroSerde<shoe_orders> ordersSerde,
+                                  final SpecificAvroSerde<shoe_customers> customersSerde,
+                                  final SpecificAvroSerde<shoe_product> productsSerde,
+                                  final SpecificAvroSerde<shoe_orders_customers> ordersCustomersSerde,
+                                  final SpecificAvroSerde<shoe_orders_customers_products> ordersCustomersProductsSerde)
+    {
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        final String ordersTopic = allProps.getProperty("orders.topic.name");
+        final String customersTopic = allProps.getProperty("customers.topic.name");
+        final String productsTopic = allProps.getProperty("products.topic.name");
+        final String outputTopic = allProps.getProperty("output.topic.name");
+
+        KStream<String, shoe_orders> ordersStream = builder.stream(ordersTopic, Consumed.with(Serdes.String(),
+                ordersSerde));
+        KTable<String, shoe_customers> customersTable = builder.table(customersTopic, Consumed.with(Serdes.String(),
+                customersSerde));
+        KTable<String, shoe_product> productsTable = builder.table(productsTopic, Consumed.with(Serdes.String(),
+                productsSerde));
+
+        OrdersCustomerJoiner ordersCustomerJoiner = new OrdersCustomerJoiner();
+        OrdersCustomerProductJoiner ordersCustomerProductJoiner = new OrdersCustomerProductJoiner();
+
+        KStream<String,shoe_orders_customers> ordersCustomersStream= ordersStream.
+                selectKey( (orderID, order) -> order.getCustomerId() ).
+                leftJoin(customersTable,ordersCustomerJoiner);
+
+        KStream<String,shoe_orders_customers_products> ordersCustomersProductsStream= ordersCustomersStream.
+                selectKey( (orderID, order) -> order.getProductId() ).
+                leftJoin(productsTable,ordersCustomerProductJoiner);
+
+        ordersCustomersProductsStream.to(outputTopic, Produced.with(Serdes.String(),
+                ordersCustomersProductsSerde));
+
+        return builder.build();
+    }
+
+
+    public static Properties loadEnvProperties(String fileName) throws IOException {
+        Properties allProps = new Properties();
+        FileInputStream input = new FileInputStream(fileName);
+        allProps.load(input);
+        input.close();
+
+        return allProps;
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            args = new String[]{DEV_CONFIG_FILE};
+        }
+
+        new App().runRecipe(args[0]);
+    }
+
+    private void runRecipe(final String configPath) throws IOException {
+        final Properties allProps = new Properties();
+        try (InputStream inputStream = new FileInputStream(configPath)) {
+            allProps.load(inputStream);
+        }
+        allProps.put(StreamsConfig.APPLICATION_ID_CONFIG, allProps.getProperty("application.id"));
+
+        final Topology topology = this.buildTopology(allProps, this.buildOrdersSerde(allProps),
+                this.buildCustomersSerde(allProps),
+                this.buildProductsSerde(allProps),
+                this.buildOrdersCustomersSerde(allProps),
+                this.buildOrdersCustomersProductsSerde(allProps));
+
+        System.out.println("Printing the Kafka Streams Topology");
+        System.out.println(topology.describe().toString());
+
+        final KafkaStreams streams = new KafkaStreams(topology, allProps);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // Attach shutdown handler to catch Control-C.
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close(Duration.ofSeconds(5));
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.cleanUp();
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        } finally {
+            streams.close();
+        }
+
+    }
+}
